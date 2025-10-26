46c251ed260e19516fe3b5a292c997837145dbec
talibg <talibguyani@gmail.com>
2025-10-26 13:56:26 +0000

    adds favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,18 @@ const firaCode = Fira_Code({
 export const metadata: Metadata = {
     title: 'TypeSnip',
     description: 'a private, local TypeScript snippet manager',
+    icons: {
+        icon: [
+            { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+            { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+            { url: '/favicon.ico' },
+        ],
+        apple: [{ url: '/apple-touch-icon.png', sizes: '180x180' }],
+        other: [
+            { rel: 'icon', url: '/android-chrome-192x192.png' },
+            { rel: 'icon', url: '/android-chrome-512x512.png' },
+        ],
+    },
     manifest: '/manifest.webmanifest',
 }
 
